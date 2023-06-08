2debfdedd8613f4fd713c201d8db419f615cc050
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-06-08 13:37:16 +0100

    created a function that will generate the random recipes and add them to html
    Relates #4

--- js/main.js
+++ js/main.js
@@ -13,7 +13,38 @@ console.log('singleRecipe', singleRecipe)
 const recipeById = await getRecipeById(singleRecipe)
 console.log('recipeById', recipeById)
 
+//Random recipes section
 const twentyRandomRecipes = await getTwentyRandomRecipes()
 console.log('twentyRandomRecipes', twentyRandomRecipes)
 
-console.log('working')
+//Get the name of each recipe
+const labelRandomRecipes = twentyRandomRecipes.hits.map(hit => hit.recipe.label)
+console.log(labelRandomRecipes)
+
+const imageRandomRecipes = twentyRandomRecipes.hits.map(hit => hit.recipe.image)
+console.log(imageRandomRecipes)
+
+const containerRandomSection = document.querySelector('.container');
+
+const createDiv = (image, label) => {
+
+  const imgDiv = document.createElement('div');
+
+  const imgRecipe = document.createElement('img');
+  imgDiv.appendChild(imgRecipe)
+  imgRecipe.src = image;
+
+  const labelDiv = document.createElement('div');
+  const labelText = document.createElement('p');
+  labelDiv.appendChild(labelText);
+  labelText.innerHTML = label;
+
+  containerRandomSection.appendChild(imgDiv)
+  containerRandomSection.appendChild(labelDiv)
+
+}
+
+imageRandomRecipes.forEach((image, index) => {
+  const label = labelRandomRecipes[index];
+  createDiv(image, label);
+});
