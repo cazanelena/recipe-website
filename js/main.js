42d51c04a512230639f4d814ee890da74135c7f4
Matthew Powell <matt.powell2784@gmail.com>
2023-06-08 12:55:40 +0100

    nav bar design added

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@ import {
   getTwentyRandomRecipes,
 } from './apiRoutes.js'
 
-const getSpanish = await getRecipes('spanish')
-console.log('getSpanish', getSpanish)
+// const getSpanish = await getRecipes('spanish')
+// console.log('getSpanish', getSpanish)
 
-const singleRecipe = getSpanish.hits[0].recipe.uri.split('_')[1]
-console.log('singleRecipe', singleRecipe)
+// const singleRecipe = getSpanish.hits[0].recipe.uri.split('_')[1]
+// console.log('singleRecipe', singleRecipe)
 
-const recipeById = await getRecipeById(singleRecipe)
-console.log('recipeById', recipeById)
+// const recipeById = await getRecipeById(singleRecipe)
+// console.log('recipeById', recipeById)
 
-const twentyRandomRecipes = await getTwentyRandomRecipes()
-console.log('twentyRandomRecipes', twentyRandomRecipes)
+// const twentyRandomRecipes = await getTwentyRandomRecipes()
+// console.log('twentyRandomRecipes', twentyRandomRecipes)
 
-console.log('working')
+// console.log('working')
