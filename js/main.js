fa776f61ca18af29da6de5a73867a8d7f97b3e83
Matthew Powell <matt.powell2784@gmail.com>
2023-06-08 11:38:50 +0100

    commit initial ideas

---
+++ js/main.js
@@ -0,0 +1,17 @@
+import {
+  getRecipes,
+  getRecipeById,
+  getTwentyRandomRecipes,
+} from './apiRoutes.js'
+
+const getSpanish = await getRecipes('spanish')
+console.log('getSpanish', getSpanish)
+
+const singleRecipe = getSpanish.hits[0].recipe.uri.split('_')[1]
+console.log('singleRecipe', singleRecipe)
+
+const recipeById = await getRecipeById(singleRecipe)
+console.log('recipeById', recipeById)
+
+const twentyRandomRecipes = await getTwentyRandomRecipes()
+console.log('twentyRandomRecipes', twentyRandomRecipes)
