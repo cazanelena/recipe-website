f2c68b40ea5142e084877fb8f7f0cb4f747724c8
Matthew Powell <matt.powell2784@gmail.com>
2023-06-09 12:24:10 +0100

    updates card design

--- js/searchResults/createSearchResults.js
+++ js/searchResults/createSearchResults.js
@@ -1,23 +1,65 @@
 import { createElement } from '../utils/createElement.js'
 import { cuisineDataList } from '../cusine/cuisineData.js'
 
+const getDifficulty = (totalTime) => {
+  if (totalTime > 90) return 5
+  if (totalTime > 60) return 4
+  if (totalTime > 45) return 3
+  if (totalTime > 30) return 2
+  return 1
+}
+
 const createSearchResultsDom = (recipeSearchResults) => {
   const recipeArray = recipeSearchResults[0]
+
   recipeArray.forEach((recipe) => {
     const recipeDetails = recipe.recipe
-    console.log('recipeDetails', recipeDetails)
 
-    const recipeImageUrl = recipeDetails.images.REGULAR.url
+    const { totalTime, label } = recipeDetails
+    const recipeLabel = label.split(' ').slice(0, 2).join(' ')
+
+    const difficulty = getDifficulty(totalTime)
+    const recipeImageUrl = recipeDetails.images.SMALL.url
 
     const recipeImage = createElement('img', {
       class: 'recipe__image',
       src: recipeImageUrl,
     })
 
+    const title = createElement(
+      'p',
+      {
+        class: 'recipe__text',
+      },
+      recipeLabel
+    )
+
+    const starsContainer = createElement('div', {
+      class: 'recipe__stars-container',
+    })
+
+    const stars = new Array(difficulty).fill('star')
+    
+    for (let i = 0; i < stars.length; i++) {
+      const starElement = createElement('img', {
+        class: 'recipe__star',
+        src: '../../img/star.svg',
+      })
+      starsContainer.appendChild(starElement)
+    }
+
+    const lemonImage = createElement('img', {
+      class: 'recipe__lemon',
+      src: '../../img/i_love_eat_lemon.svg',
+    })
+
     const recipeContainer = createElement(
-      'div',
+      'article',
       { class: 'recipe__container' },
-      recipeImage
+      recipeImage,
+      title,
+      starsContainer,
+      lemonImage
     )
 
     const recipesSection = document.querySelector('#recipes')
