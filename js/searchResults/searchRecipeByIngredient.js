524fd99db60461d90a800d4cca833b05ec67dfcb
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-06-16 15:00:31 +0100

    added some more css

--- js/searchResults/searchRecipeByIngredient.js
+++ js/searchResults/searchRecipeByIngredient.js
@@ -1,14 +1,14 @@
 export const addSearchEventListener = () => {
-  const submitButton = document.getElementById('search')
+  const submitButton = document.getElementById("search");
 
-  submitButton.addEventListener('click', async function () {
-    let searchIngredientResult = document.getElementById('searchBar').value
-    console.log()
-    const userInput = searchIngredientResult.toLowerCase().trim()
-    console.log(userInput)
-    
-    document.getElementById('form-text').reset();
-    sessionStorage.setItem('searchTerm', JSON.stringify(userInput))
-    window.location.href = './search-results.html'
-  })
-}
+  submitButton.addEventListener("click", async function () {
+    let searchIngredientResult = document.getElementById("searchBar").value;
+    console.log();
+    const userInput = searchIngredientResult.toLowerCase().trim();
+    console.log(userInput);
+
+    document.getElementById("form-text").reset();
+    sessionStorage.setItem("searchTerm", JSON.stringify(userInput));
+    window.location.href = "./search-results.html";
+  });
+};
